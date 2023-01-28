a0aecfcde953f458a087dac47ca90bb796c43b42
Derek Bailey <derekbailey@google.com>
2023-01-27 17:02:19 -0800

    simplified CreateString<> to just provide the offset type

--- src/idl_gen_cpp.cpp
+++ src/idl_gen_cpp.cpp
@@ -2995,8 +2995,7 @@ class CppGenerator : public BaseGenerator {
               code_.SetValue(
                   "CREATE_STRING",
                   "CreateString" + std::string(field->offset64
-                                                   ? "<::flatbuffers::Offset64<"
-                                                     "::flatbuffers::String>>"
+                                                   ? "<::flatbuffers::Offset64>"
                                                    : ""));
             } else {
               code_.SetValue("CREATE_STRING", "CreateSharedString");
@@ -3284,8 +3283,7 @@ class CppGenerator : public BaseGenerator {
         if (!field.shared) {
           code += "_fbb.CreateString" +
                   std::string(field.offset64
-                                  ? "<std::string, ::flatbuffers::Offset64<"
-                                    "::flatbuffers::String>>"
+                                  ? "<::flatbuffers::Offset64>"
                                   : "") +
                   "(";
         } else {
